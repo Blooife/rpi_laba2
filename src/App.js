f8fb203e3687b1ad1afb8dbbb3a9acdd59dbc4fd
loziuk <loziukvika10@gmail.com>
2022-12-10 15:37:13 +0300

    first_try

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Девочки, я люблю вас
         </a>
       </header>
     </div>
