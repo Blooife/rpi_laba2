7ebc40bd1714a3e9e7d288dbde52189175ccfabb
loziuk <loziukvika10@gmail.com>
2022-12-10 16:32:34 +0300

    hui

---
+++ src/site.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function site(){
+    return(
+        /*<BrowserRouter>
+          <Routes>
+            <Route path="/" element={<NavigationBar />}>
+              <Route path="" element={<Home />} />
+              <Route path="architects" element={<Architects />} />
+                <Route path="architects/:id" element={<Architect />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>*/
+        <header className="App-header">
+            Девочки, я вас люблю!
+        </header>
+        
+    );
+}
+export default site;
