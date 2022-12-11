f94907b617d59d1d4bfd0dddd9fa7d8b6e08e40b
Blooife <beusas.23@gmail.com>
2022-12-11 19:16:31 +0300

    im sorre

--- src/App.js
+++ src/App.js
@@ -6,7 +6,93 @@ import {BrowserRouter, useParams, Route, Routes,Link} from "react-router-dom";
 //import Architect from "./pages/Architect";
 
 import { Timeline, TimelineItem }  from 'vertical-timeline-component-for-react';
+//<Timeline/>
+const PhotographersAPI = {
+  photographers: [
+    { number: 1, name: "Антон Мотолько", fileName: "anton_motolko"},
+    { number: 2, name: "Бенедикт Генрик Тышкевич", fileName: "benedikt_genrik_tyshkevich"},
+    { number: 3, name: "Лев Дашкевич", fileName: "lev_dashkevich"},
+    { number: 4, name: "Сергей Колтович", fileName: "sergei_koltovich"},
+    { number: 5, name: "Ян Булгак", fileName: "yan_bulgak"}
+  ],
+  phData: [
+    {number: 1, birthYear: "1914",deathYear: "1990",birthDate: "13.01.1914",deathDate: "23.01.1990",
+    events: [
+      "1914 - 1932",
+      "1932 - 1936",
+      "1936 - 1940",
+      "1940 - 1944",
+      "1944 - 1950",
+      "1950 - 1990"],
+    gallery : ["1.png","2.png","3.png"],
+    videoLink: "7CsnFSbpUqE"},
 
+    {number: 2, birthYear: "1852",deathYear: "1935",birthDate: "11.12.1852",deathDate: "1935",
+    events: [
+      "1852 - 1874",
+      "1874 - 1876",
+      "1876 - 1894",
+      "1894 - 1899",
+      "1899 - 1935",
+      "1935 - 1999",
+      "1999 - 2009"],
+      gallery : ["1.jpg","2.jpg","3.jpg","4.jpg"],
+    videoLink: "7CsnFSbpUqE"},
+
+    {number: 3, birthYear: "1914",deathYear: "1990",birthDate: "13.01.1914",deathDate: "23.01.1990",
+    events: [
+      "1914 - 1932",
+      "1932 - 1936",
+      "1936 - 1940",
+      "1940 - 1944",
+      "1944 - 1950",
+      "1950 - 1990"],
+    gallery : ["1.jpg","2.jpg","3.jpg","4.jpg"],
+    videoLink: "7CsnFSbpUqE"},
+
+    {number: 4, birthYear: "1914",deathYear: "1990",birthDate: "13.01.1914",deathDate: "23.01.1990",
+    events: [
+      "1914 - 1932",
+      "1932 - 1936",
+      "1936 - 1940",
+      "1940 - 1944",
+      "1944 - 1950",
+      "1950 - 1990"],
+    gallery : ["1.png","2.png","3.png"],
+    videoLink: "7CsnFSbpUqE"},
+
+    {number: 5, birthYear: "1914",deathYear: "1990",birthDate: "13.01.1914",deathDate: "23.01.1990",
+    events: [
+      "1914 - 1932",
+      "1932 - 1936",
+      "1936 - 1940",
+      "1940 - 1944",
+      "1944 - 1950",
+      "1950 - 1990"],
+    gallery : ["1.png","2.png","3.png"],
+    videoLink: "7CsnFSbpUqE"},
+  ],
+  all: function() { return this.photographers},
+}
+
+function Card(){
+    const params = useParams();
+    const id = params.number;
+
+    const photographer = PhotographersAPI.photographers.find(p=>p.number == id);
+    if(photographer===undefined){
+    return <div>Sorry, but the photographer was not found</div>
+    }
+    return (
+    <div>
+      <img src={require(`./data/${photographer.fileName}/portrait.jpg`)} />
+      <h1>{photographer.name} (#{photographer.number})</h1>
+      <Link to='/List'>Вернуться к списку</Link>
+      <Timeline/>
+    </div>
+
+    );
+}
 const List = () => (
   <div>
     <ul>
