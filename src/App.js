1ee78da5b4c2638461661a0fc0ba88592f675b82
loziuk <loziukvika10@gmail.com>
2022-12-11 04:02:12 +0300

    eeeeeeeeee

--- src/App.js
+++ src/App.js
@@ -1,18 +1,112 @@
 //import './App.css';
 //import Home from "./pages/Home";
-import {BrowserRouter, RedirectFunction, Route, Routes} from "react-router-dom";
+import {BrowserRouter, RedirectFunction, Route, Routes,Link} from "react-router-dom";
 //import NavigationBar from "./components/Navigation/NavigationBar";
 //import Architects from "./pages/Architects";
 //import Architect from "./pages/Architect";
+
+const PlayerAPI = {
+  players: [
+    { number: 1, name: "Ben Blocker", position: "G" },
+    { number: 2, name: "Dave Defender", position: "D" },
+    { number: 3, name: "Sam Sweeper", position: "D" },
+    { number: 4, name: "Matt Midfielder", position: "M" },
+    { number: 5, name: "William Winger", position: "M" },
+    { number: 6, name: "Fillipe Forward", position: "F" }
+  ],
+  all: function() { return this.players},
+  get: function(id) {
+    const isPlayer = p => p.number === id
+    return this.players.find(isPlayer)
+  }
+}
+
+// The FullRoster iterates over all of the players and creates
+// a link to their profile page.
+const FullRoster = () => (
+  <div>
+    <ul>
+      {
+        PlayerAPI.all().map(p => (
+          <li key={p.number}>
+            <Link to={`/roster/${p.number}`}>{p.name}</Link>
+          </li>
+        ))
+      }
+    </ul>
+  </div>
+)
+
+// The Player looks up the player using the number parsed from
+// the URL's pathname. If no player is found with the given
+// number, then a "player not found" message is displayed.
+
+// The Roster component matches one of two different routes
+// depending on the full pathname
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>6/5 @ Evergreens</li>
+      <li>6/8 vs Kickers</li>
+      <li>6/14 @ United</li>
+    </ul>
+  </div>
+)
+function Player(props) {
+  return(
+    <div>
+      <h1>Player!</h1>
+      <Link to='/roster'>Back</Link>
+  </div>
+  );
+  
+}
+
+const Home = () => (
+  <div>
+    <h1>Welcome to the Tornadoes Website!</h1>
+  </div>
+)
+
+// The Main component renders one of the three provided
+// Routes (provided that one matches). Both the /roster
+// and /schedule routes will match any pathname that starts
+// with /roster or /schedule. The / route will only match
+// when the pathname is exactly the string "/"
+const Main = () => (
+  <main>
+    <Routes>
+            <Route path="/roster" element={<FullRoster />} />
+            <Route path="/roster/:number" element={<Player number/>} />
+            <Route path="/schedule" element={<Schedule />} />
+            <Route path="/" element={<Home />}>
+            </Route>
+    </Routes>
+  </main>
+)
+
+// The Header creates links that can be used to navigate
+// between routes.
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link to='/'>Home</Link></li>
+        <li><Link to='/roster'>Roster</Link></li>
+        <li><Link to='/schedule'>Schedule</Link></li>
+      </ul>
+    </nav>
+  </header>
+)
 
 function App() {
   return (
       <>
         <BrowserRouter>
           <div>
-            <p>
-              HUI
-            </p>
+          <Header />
+          <Main />
           </div>
         </BrowserRouter>
       </>
